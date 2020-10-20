78abb033ff0ea397437378de25f0d9ac9b69d73b
abba <abba@nuclearinstruments.eu>
2020-10-20 18:01:55 +0200

    rules

---
+++ SciDKSDK_test/Debug/main.c.o.d
@@ -0,0 +1,6 @@
+Debug/main.c.o: main.c ../libSCIDK/include/SCIDK_API_C.h \
+ ../libSCIDK/include/NIErrorCode.h
+
+../libSCIDK/include/SCIDK_API_C.h:
+
+../libSCIDK/include/NIErrorCode.h:
---
+++ libSCIDK/Debug/src_SCIDK_API_C.cpp.o.d
@@ -0,0 +1,14 @@
+Debug/src_SCIDK_API_C.cpp.o: src/SCIDK_API_C.cpp \
+ src/../include/SCIDK_API_C.h src/../include/NIErrorCode.h \
+ src/../include/FTDI_CPP_DLL.h src/../include/ftd2xx.h \
+ src/../include/WinTypes.h
+
+src/../include/SCIDK_API_C.h:
+
+src/../include/NIErrorCode.h:
+
+src/../include/FTDI_CPP_DLL.h:
+
+src/../include/ftd2xx.h:
+
+src/../include/WinTypes.h:
